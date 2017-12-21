888dfb6aa29ba161b714754ca9b5da911ade60a7
DicheBach <dichebach@gmail.com>
2017-12-21 09:51:26 -0500

    beginning of dichebach branch

--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -73,6 +73,6 @@ private:
 	D3D11_MAPPED_SUBRESOURCE							mappedSysBufferTexture;
 	Color*                                              pSysBuffer = nullptr;
 public:
-	static constexpr int ScreenWidth = 800;
-	static constexpr int ScreenHeight = 600;
+	static constexpr int ScreenWidth = 800; //this is the game pane width
+	static constexpr int ScreenHeight = 600; //this is the game pane height
 };
